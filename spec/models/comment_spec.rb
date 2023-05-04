4b62a20f3bf79b296d2d6e8b83a41609d828c2c6
Ermi <manermidem@gmail.com>
2023-05-04 20:23:30 +0300

    Add a test for comment model

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,4 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
 
+    subject(:user) { User.new(name: 'Ermiyas Demsew', postsCounter: 0) }
+
+    subject(:post) { Post.new(author: user, title: 'This is a test for post model using tdd', commentsCounter: 0, likesCounter: 0) }
+
+    subject(:comment) { Comment.new(author: user, post: post, text: 'This is a test for comment model using tdd') }
+
+    before { comment.save }
+    
+        it 'should be valid' do
+             expect(comment).to be_valid
+        end
 end
