dfa856a56d37c94db566595184b5e8475acc4bce
Ermi <manermidem@gmail.com>
2023-05-04 18:30:45 +0300

    Add validation for likesCounter in post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,7 @@ class Post < ApplicationRecord
 
   validates :title, presence: true, length: { maximum: 250 }
   validates :commentsCounter, presence: true, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+  validates :likesCounter, presence: true, numericality: { greater_than_or_equal_to: 0, only_integer: true }
 
   after_save :update_post_counter_cache
 
