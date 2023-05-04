1bf3604c811aaaae78a588214d85ef96169755d0
Ermi <manermidem@gmail.com>
2023-05-04 20:26:32 +0300

    Update schema file

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# rubocop:disable Metrics/BlockLength
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,53 +10,52 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_504_060_538) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_04_060538) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'comments', force: :cascade do |t|
-    t.text 'text'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.bigint 'author_id'
-    t.bigint 'post_id'
-    t.index ['author_id'], name: 'index_comments_on_author_id'
-    t.index ['post_id'], name: 'index_comments_on_post_id'
+  create_table "comments", force: :cascade do |t|
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "author_id"
+    t.bigint "post_id"
+    t.index ["author_id"], name: "index_comments_on_author_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
-  create_table 'likes', force: :cascade do |t|
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.bigint 'author_id'
-    t.bigint 'post_id'
-    t.index ['author_id'], name: 'index_likes_on_author_id'
-    t.index ['post_id'], name: 'index_likes_on_post_id'
+  create_table "likes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "author_id"
+    t.bigint "post_id"
+    t.index ["author_id"], name: "index_likes_on_author_id"
+    t.index ["post_id"], name: "index_likes_on_post_id"
   end
 
-  create_table 'posts', force: :cascade do |t|
-    t.string 'title'
-    t.text 'text'
-    t.integer 'commentsCounter'
-    t.integer 'likesCounter'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.bigint 'author_id'
-    t.index ['author_id'], name: 'index_posts_on_author_id'
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.integer "commentsCounter"
+    t.integer "likesCounter"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "author_id"
+    t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'name'
-    t.string 'photo'
-    t.text 'bio'
-    t.integer 'postsCounter'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "photo"
+    t.text "bio"
+    t.integer "postsCounter"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_foreign_key 'comments', 'posts'
-  add_foreign_key 'comments', 'users', column: 'author_id'
-  add_foreign_key 'likes', 'posts'
-  add_foreign_key 'likes', 'users', column: 'author_id'
-  add_foreign_key 'posts', 'users', column: 'author_id'
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users", column: "author_id"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users", column: "author_id"
+  add_foreign_key "posts", "users", column: "author_id"
 end
-# rubocop:enable Metrics/BlockLength
