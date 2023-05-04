c1044d4c53d6ccdd702329f84e461c721530a5d5
Ermi <manermidem@gmail.com>
2023-05-04 09:07:09 +0300

    Rename the column name and update the schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_04_055008) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_04_060538) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,7 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_04_055008) do
     t.string "name"
     t.string "photo"
     t.text "bio"
-    t.integer "postCounter"
+    t.integer "postsCounter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
