e9dc225a07cbd832e7dbf49dee2a8948057d5f5d
Ermi <manermidem@gmail.com>
2023-05-04 14:19:38 +0300

    Add method to return recent lists of posts by user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments
   has_many :likes
+
+
+  # A method to return a recent list of posts by this user
+  def recent_posts
+    posts.order(created_at: :desc).limit(3)
+  end
 end
