6e77a072fa75e7ff37a961c56568a229da21f1d6
Ermi <manermidem@gmail.com>
2023-05-04 20:26:13 +0300

    Add a test for like model

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -2,4 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Like, type: :model do
 
+
+    subject(:user) { User.new(name: 'Ermiyas Demsew', postsCounter: 0) }
+
+    subject(:post) { Post.new(author: user, title: 'This is a test for post model using tdd', commentsCounter: 0, likesCounter: 0) }
+
+    subject(:like) { Like.new(author: user, post: post) }
+
+    before { like.save }
+
+        it 'should be valid' do
+             expect(like).to be_valid
+        end
 end
