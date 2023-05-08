ab3d503c1117a3661602e8e7aca579754f64cc85
Ermi <manermidem@gmail.com>
2023-05-08 21:29:25 +0300

    Edit routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'users#index'
 
-  get '/users', to: 'users#index'
+  resources :users do
+    resources :posts do
+    resources :comments
+  end
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
