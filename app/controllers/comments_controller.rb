3f46858a63469c874fedc683bedb103dff7ea3d0
Ermi <manermidem@gmail.com>
2023-05-08 21:28:27 +0300

    Add a method for the comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,10 @@
 class CommentsController < ApplicationController
+
+    def index
+      @comments = Comment.all
+    end
+
+    def show
+        @comment = Comment.find(params[:id])
+    end
 end
