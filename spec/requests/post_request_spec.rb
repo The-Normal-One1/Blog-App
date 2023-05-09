31414c9cf78ce7323c707b9ba650a1a5e2091690
Ermi <manermidem@gmail.com>
2023-05-09 07:31:04 +0300

    Edit request test for index & show action in post.

--- spec/requests/post_request_spec.rb
+++ spec/requests/post_request_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe "Posts", type: :request do
+    describe "GET /posts" do
+        it "returns http success" do
+        get "/posts"
+        #test for http response status
+        expect(response).to have_http_status(:success)
+        expect(response.status).to eq(200)
+        #correct response is rendered
+        expect(response).to render_template(:index)
+        # include correct placeholders
+        expect(response.body).to include("All posts")
+        expect(response.body).to include("Hello")
+        expect(response.body).to include("This is the second post!!")
+        end
+    end
 
+    describe "GET /posts/:id" do
+        it "returns http success" do
+        get "/users/1/posts/1"
+        #test for http response status
+        expect(response).to have_http_status(:success)
+        expect(response.status).to eq(200)
+        #correct response is rendered
+        expect(response).to render_template(:show)
+        # include correct placeholders
+        expect(response.body).to include("Hello")
+        end
+    end
 end
