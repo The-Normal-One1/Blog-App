af3d907553452f9fbb457a78a97a853ec586a752
Ermi <manermidem@gmail.com>
2023-05-09 07:40:28 +0300

    Fix linters

--- spec/requests/comment_request_spec.rb
+++ spec/requests/comment_request_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe "Comments", type: :request do
-    describe "GET /comments" do
-        it "returns http success" do
-        get "/users/1/posts/1/comments"
-        #test for http response status
-        expect(response).to have_http_status(:success)
-        expect(response).to have_http_status(200)
-        expect(response.status).to eq(200)
-        #correct response is rendered
-        expect(response).to render_template(:index)
-        # include correct placeholders
-        expect(response.body).to include("All Comments")
-        expect(response.body).to include("How are you doing my friend!")
-        expect(response.body).to include("I will send you the documents to read")
-        end
+RSpec.describe 'Comments', type: :request do
+  describe 'GET /comments' do
+    it 'returns http success' do
+      get '/users/1/posts/1/comments'
+      # test for http response status
+      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(200)
+      expect(response.status).to eq(200)
+      # correct response is rendered
+      expect(response).to render_template(:index)
+      # include correct placeholders
+      expect(response.body).to include('All Comments')
+      expect(response.body).to include('How are you doing my friend!')
+      expect(response.body).to include('I will send you the documents to read')
     end
+  end
 end
--- spec/requests/user_request_spec.rb
+++ spec/requests/user_request_spec.rb
@@ -1,33 +1,33 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-    describe "GET /users" do
-        it "returns http success" do
-        get "/users"
-        #test for http response status
-        expect(response).to have_http_status(:success)
-        expect(response).to have_http_status(200)
-        expect(response.status).to eq(200)
-        #correct response is rendered
-        expect(response).to render_template(:index)
-        # include correct placeholders
-        expect(response.body).to include("Users")
-        expect(response.body).to include("Ermiyas")
-        expect(response.body).to include("Tom")
-        end
+RSpec.describe 'Users', type: :request do
+  describe 'GET /users' do
+    it 'returns http success' do
+      get '/users'
+      # test for http response status
+      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(200)
+      expect(response.status).to eq(200)
+      # correct response is rendered
+      expect(response).to render_template(:index)
+      # include correct placeholders
+      expect(response.body).to include('Users')
+      expect(response.body).to include('Ermiyas')
+      expect(response.body).to include('Tom')
     end
+  end
 
-    describe "GET /users/:id" do
-        it "returns http success" do
-        get "/users/1"
-        #test for http response status
-        expect(response).to have_http_status(:success)
-        expect(response).to have_http_status(200)
-        expect(response.status).to eq(200)
-        #correct response is rendered
-        expect(response).to render_template(:show)
-        # include correct placeholders
-        expect(response.body).to include("Tom")
-        end
+  describe 'GET /users/:id' do
+    it 'returns http success' do
+      get '/users/1'
+      # test for http response status
+      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(200)
+      expect(response.status).to eq(200)
+      # correct response is rendered
+      expect(response).to render_template(:show)
+      # include correct placeholders
+      expect(response.body).to include('Tom')
     end
+  end
 end
