972cf9f89a59b9089683df3abf55b508a9d30404
Ermi <manermidem@gmail.com>
2023-05-09 07:09:35 +0300

    Add testing examples to check the index action

--- spec/requests/user_request_spec.rb
+++ spec/requests/user_request_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-   
+    describe "GET /users" do
+        it "returns http success" do
+        get "/users"
+        #test for http response status
+        expect(response).to have_http_status(:success)
+        expect(response).to have_http_status(200)
+        expect(response.status).to eq(200)
+        #correct data is rendered and include correct placeholders
+        expect(response.body).to include("Users")
+        expect(response.body).to include("Ermiyas")
+        expect(response.body).to include("Tom")
+        end
+    end
 end
