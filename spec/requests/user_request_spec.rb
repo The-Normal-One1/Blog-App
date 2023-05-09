d310e9ce00a621fb708e3f7a0b8b2f06a8106893
Ermi <manermidem@gmail.com>
2023-05-09 07:19:53 +0300

    Edit user_request testing for index & show actions

--- spec/requests/user_request_spec.rb
+++ spec/requests/user_request_spec.rb
@@ -8,9 +8,25 @@ RSpec.describe "Users", type: :request do
         expect(response).to have_http_status(:success)
         expect(response).to have_http_status(200)
         expect(response.status).to eq(200)
-        #correct data is rendered and include correct placeholders
+        #correct response is rendered
+        expect(response).to render_template(:index)
+        # include correct placeholders
         expect(response.body).to include("Users")
         expect(response.body).to include("Ermiyas")
+        expect(response.body).to include("Tom")
+        end
+    end
+
+    describe "GET /users/:id" do
+        it "returns http success" do
+        get "/users/1"
+        #test for http response status
+        expect(response).to have_http_status(:success)
+        expect(response).to have_http_status(200)
+        expect(response.status).to eq(200)
+        #correct response is rendered
+        expect(response).to render_template(:show)
+        # include correct placeholders
         expect(response.body).to include("Tom")
         end
     end
