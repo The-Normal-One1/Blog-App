04c579135416dea2ce15fe3b8b59f09783126c6d
Ermi <manermidem@gmail.com>
2023-05-10 14:35:15 +0300

    Edit test for comment controller

--- spec/requests/comment_request_spec.rb
+++ spec/requests/comment_request_spec.rb
@@ -3,17 +3,26 @@ require 'rails_helper'
 RSpec.describe 'Comments', type: :request do
   describe 'GET /comments' do
     it 'returns http success' do
-      get '/users/1/posts/1/comments'
+      get '/users/1/posts/443/comments'
       # test for http response status
       expect(response).to have_http_status(:success)
       expect(response).to have_http_status(200)
       expect(response.status).to eq(200)
+    end
+
+    it 'response is rendered with the correct template' do
+      get '/users/1/posts/443/comments'
       # correct response is rendered
       expect(response).to render_template(:index)
+    end
+
+    it 'response body includes all comments' do
+      get '/users/1/posts/443/comments'
       # include correct placeholders
       expect(response.body).to include('All Comments')
-      expect(response.body).to include('How are you doing my friend!')
-      expect(response.body).to include('I will send you the documents to read')
+      expect(response.body).to include('I enjoyed watching football games!!')
+      expect(response.body).to include('I like front end!!')
     end
+
   end
 end
