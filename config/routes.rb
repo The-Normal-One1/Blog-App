efbdf29973d0477389cb4946d4ddcf7f46a5fe5c
Ermi <manermidem@gmail.com>
2023-05-12 21:44:40 +0300

    Edit routing for comments

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index show new create] do
-      resources :comments
+      resources :comments, only: %i[new create]
     end
   end
   # Defines the root path route ("/")
