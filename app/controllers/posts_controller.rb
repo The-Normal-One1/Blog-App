11ecc56cae617b92e0e1a0e85c74b95d4cb94fe6
Ermi <manermidem@gmail.com>
2023-05-12 20:21:11 +0300

    Add a method for new & create in posts_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,24 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+ 
+  def new
+    @user = User.find(params[:user_id])
+    @post = Post.new
+  end
+  
+  def create
+    #using the current_user method from ApplicationController
+  @post = current_user.posts.new(post_params)
+  if @post.save
+    redirect_to user_post_path(current_user.id, @post.id)
+  else
+    render :new
+  end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :text).merge(author_id: current_user.id, commentsCounter: 0, likesCounter: 0)
+  end
 end
