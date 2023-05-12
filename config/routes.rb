7183e4b0b752a77655f4a8a5a7fa9d3d8a816c75
Ermi <manermidem@gmail.com>
2023-05-12 22:26:36 +0300

    Edit routing for the likes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
 
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index show new create] do
-      resources :comments, only: %i[new create]
+      resources :comments, only: %i[new create] 
+      resources :likes, only: %i[create]
     end
   end
   # Defines the root path route ("/")
