4c0d65aab64914984e98c941a3e46af55f0eb439
Ermi <manermidem@gmail.com>
2023-05-18 07:56:21 +0300

    Delete unwanted files

---
+++ db/migrate/20230518045455_remove_password_columns_from_users.rb
@@ -0,0 +1,8 @@
+class RemovePasswordColumnsFromUsers < ActiveRecord::Migration[7.0]
+  def change
+    remove_column :users, :encrypted_password, :string
+    remove_column :users, :reset_password_token, :string
+    remove_column :users, :reset_password_sent_at, :datetime
+    remove_column :users, :remember_created_at, :datetime
+  end
+end
