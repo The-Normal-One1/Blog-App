fc5f790d77f2839a73b1e0402e74925174d71452
Ermi <manermidem@gmail.com>
2023-05-19 22:28:33 +0300

    Add methods for posts controllers api

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,2 +1,49 @@
 class Api::V1::PostsController < ApplicationController
+
+    # GET /api/v1/posts
+    def index
+        @posts = Post.includes(:author, :comments, :likes).all
+        render json: @posts
+    end
+    
+    # GET /api/v1/posts/:id
+    def show
+        @post = Post.find(params[:id])
+        render json: @post
+    end
+    
+    # POST /api/v1/posts
+    def create
+        @post = Post.new(post_params)
+        if @post.save
+        render json: { status: :created, post: @post, notice: 'Post was successfully created.', location: @post }
+        else
+        render json: { status: :unprocessable_entity, errors: @post.errors }
+        end
+    end
+
+    # PATCH/PUT /api/v1/posts/:id
+    def update
+        @post = Post.find(params[:id])
+        if @post.update(post_params)
+        render json: { status: :ok, post: @post, notice: 'Post was successfully updated.', location: @post }
+        else
+        render json: { status: :unprocessable_entity, errors: @post.errors }
+        end
+
+    end
+    
+    # DELETE /api/v1/posts/:id
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+        render json: { status: :ok }
+    end
+    
+    private
+    
+    def post_params
+        params.require(:post).permit(:title, :body, :author_id)
+    end
+
 end
